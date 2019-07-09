b84c725aa962b800d5b4b68c7aaf67d7f130fe99
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-07-09 17:38:05 +0530

    Some progress

--- js/node.js
+++ js/node.js
@@ -1,12 +1,22 @@
 function Node(i,j,cols,rows) {
-  this.x = i;
-  this.y = j;
-  this.f = 0;
+  this.i = i;
+  this.j = j;
+  this.f = 0; // f = g + h
   this.g = 0;
   this.h = 0;
+  this.neighbors = [];
+  this.previous = undefined;
+
   this.show = function(color) {
     fill(color);
     let w = width/cols, h = height/rows;
-    rect(this.x*w, this.y*h, w, h);
+    rect(i*w, j*h, w, h);
+  }
+
+  this.addNeighbors = function(grid) {
+    if(i < cols-1) {this.neighbors.push(grid[i+1][j]);}
+    if(i > 0) {this.neighbors.push(grid[i-1][j]);}
+    if(j < rows-1) {this.neighbors.push(grid[i][j+1]);}
+    if(j > 0) {this.neighbors.push(grid[i][j-1]);}
   }
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,11 @@
-let cols = 5;
-let rows = 5;
+let cols = 25;
+let rows = 25;
 let grid = new Array(cols);
 
 let openSet = []; // nodes not visited yet
 let closedSet = []; // nodes visited
 let start, end;
+
 
 function setup() {
   createCanvas(400, 400);
@@ -19,17 +20,56 @@ function setup() {
       grid[i][j] = new Node(i,j,cols,rows);
     }
   }
-
+  for(let i=0; i<cols; i++){
+    for(let j=0; j<rows; j++){
+      grid[i][j].addNeighbors(grid);
+    }
+  }
   start = grid[0][0];
   end = grid[cols-1][rows-1];
 
   openSet.push(start); // starting with open set
 }
 
+
 function draw() {
 
   if(openSet.length > 0){
-    // keep going
+    // getting element with lowest f value
+    let lowestNodeIndex = 0;
+    for(let i=0; i<openSet.length; i++) {
+      if(openSet[i].f < openSet[lowestNodeIndex].f){
+        lowestNodeIndex = i;
+      }
+    }
+    let current = openSet[lowestNodeIndex];
+
+    if(current === end){
+      console.log("DONE!");
+    }
+
+    removeFromArray(openSet, current);
+    closedSet.push(current);
+
+    let neighbors = current.neighbors;
+    for(neighbor of neighbors){
+      if(!closedSet.includes(neighbor)){
+        let tempGScore = current.g + 1;
+        if(openSet.includes(neighbor)){
+          if(tempGScore < neighbor.g){
+            neighbor.g = tempGScore;
+          }
+        } else {
+          neighbor.g = tempGScore;
+          openSet.push(neighbor);
+        }
+        neighbor.h = heuristic(neighbor, end);
+        neighbor.f = neighbor.g + neighbor.h;
+        neighbor.previous = current;
+      }
+
+    }
+
   } else {
     // no solution
   }
@@ -49,4 +89,18 @@ function draw() {
   for(let i=0; i<openSet.length; i++){
     openSet[i].show(color(0,255,0));
   }
+}
+
+
+function removeFromArray(arr, elt){
+  for(let i=arr.length-1; i>=0; i--){
+    if(arr[i] == elt){
+      arr.splice(i, 1);
+      break;
+    }
+  }
+}
+
+function heuristic(a, b){
+  return dist(a.i,a.j,b.i,b.j);
 }
